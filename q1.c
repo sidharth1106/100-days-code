c890a6e596a3d657a42fc6f43e793acf67c1dbb3
sidharth1106 <balooni.sidharth@gmail.com>
2025-09-11 23:08:37 +0530

    Add files via upload

---
+++ q1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int a,b;
+    printf("enter your numbers \n");
+    scanf("%d",&a);
+    scanf("%d",&b);
+    printf("sum is %d\n",a+b);
+    return 0;
+
+}
---
+++ q2.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int a,b;
+    printf("enter your numbers \n");
+    scanf("%d",&a);
+    scanf("%d",&b);
+    printf("sum is %d\n",a+b);    
+    printf("diff is %d\n", a-b);
+    printf("product is %d\n",a*b);
+    printf("div is %d\n",a/b);
+    return 0;
+
+}
